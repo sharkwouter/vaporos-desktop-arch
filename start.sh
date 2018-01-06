4ae6bbd7e0fefe46b2ad29b9d4bc78751493d061
Wouter Wijsman <s1106648@student.windesheim.nl>
2018-01-06 01:55:18 +0100

    Updated script quite a bit and added Dockerfile

--- start.sh
+++ start.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-docker run -t -i base/archlinux /bin/bash
+
+#Get the directory of this script
+DIR="$(realpath $0|rev|cut -d '/' -f2-|rev)"
+DOCKERFILE="$DIR/builder"
+BUILDDIR="$DIR/archlive"
+
+#Create archlive directory if it doesn't exist
+mkdir -p $BUILDDIR
+
+#Build the docker container
+cd $DOCKERFILE
+docker build -t vaporos-builder .
+cd $DIR
+
+#Run container
+docker run -t -i -v $BUILDDIR:/root/archlive vaporos-builder /bin/bash
