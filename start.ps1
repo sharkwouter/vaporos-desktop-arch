c3653818e6e68bde5a34bd16e2a43cc36f85cd4b
Kevin van de Pas <kevin@van-de-pas.nl>
2018-01-24 23:20:48 +0100

    Added initial version Windows powershell script

---
+++ start.ps1
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+
+#Get the directory of this script
+$work_dir=$PSScriptRoot 
+$dockerfile="Dockerfile"
+$output_dir="output"
+
+#Create output directory if it doesn't exist
+mkdir ${work_dir}/${output_dir}
+
+#Build the docker container
+docker build -f ${work_dir}/${dockerfile} -t vaporos-builder ${work_dir}
+
+#Run container
+docker run --privileged --rm -ti -v ${work_dir}/${output_dir}:/root/vaporos-live/out -h vaporos-builder vaporos-builder /bin/bash
